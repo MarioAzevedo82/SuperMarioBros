77237677593f38592f7f40836f4fffc3cbb04a12
Mario Azevedo <mariobct@msn.com>
2025-09-17 09:40:19 -0300

    clicouNoBotao() e clicouFora()

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao() {
+    // Torcar a posição do formulário pra ir para o centro da tela
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function clicouFora() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+}
